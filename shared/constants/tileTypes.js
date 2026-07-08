b8ec2cc0a36970a6e0cd7640c2d6b48484a05ef9
Ashomondi <adhiamboa745@gmail.com>
2026-07-08 21:57:27 +0300

    Created board cell structural definitions inside constants/tileTypes.js

---
+++ shared/constants/tileTypes.js
@@ -0,0 +1,12 @@
+export const TILE_TYPES = {
+    PROPERTY: 'property',
+    TRANSPORT_HUB: 'transport_hub',
+    UTILITY: 'utility',
+    GO: 'go',
+    JAIL: 'jail',
+    FREE_PARKING: 'free_parking',
+    GO_TO_JAIL: 'go_to_jail',
+    CHANCE: 'chance',
+    COMMUNITY_CHEST: 'community_chest',
+    TAX: 'tax'
+};
