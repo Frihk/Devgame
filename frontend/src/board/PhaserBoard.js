aa894574ead6fa2e127eb44655350f981fa3a931
sospeter57 <sospeterkinyanjui57@gmail.com>
2026-07-11 06:32:13 +0300

    chore: add PhaserBoard.js

---
+++ frontend/src/board/PhaserBoard.js
@@ -0,0 +1,11 @@
+export class Board {
+    constructor(scene) {
+        this.scene = scene;
+        this.tiles = [];
+        this.createBoardGrid();
+    }
+
+    createBoardGrid() {
+        console.log("Board mapping initialized.");
+    }
+}
